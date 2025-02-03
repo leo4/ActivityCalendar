ea7e075ce482cf98968b85a0cf05da21ca7f0c5c
Leonardo Yarto <leo.yarto@gmail.com>
2025-02-02 23:05:11 -0600

    redirected empty path to calendar app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { CalendarComponent } from './calendar/calendar.component';
 
 export const routes: Routes = [
-  { path: 'calendar', component: CalendarComponent }
+  { path: 'calendar', component: CalendarComponent },
+  { path: '', redirectTo: '/calendar', pathMatch: 'full' }
 ];
