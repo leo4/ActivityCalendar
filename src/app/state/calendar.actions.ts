d9334717928c6d97a6ff4100cbe38be0b1468b8a
leo4 <leo.yarto@gmail.com>
2025-02-02 23:17:30 -0600

    Update calendar.actions.ts

--- src/app/state/calendar.actions.ts
+++ src/app/state/calendar.actions.ts
@@ -1,7 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { CalendarEvent } from './calendar-event.model';
 
-
 export const loadEvents = createAction('[Calendar] Load Events');
 export const loadEventsSuccess = createAction(
   '[Calendar] Load Events Success',
